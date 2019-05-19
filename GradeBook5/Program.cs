27b0a899e845cd5bf87275862bb128dc4c7f6e3b
diazfrankp <diazfrankp@gmail.com>
2019-05-19 17:58:40 -0400

    Added xunit test BookTests.

--- GradeBook5/Program.cs
+++ GradeBook5/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace GradeBook5
 {
@@ -6,7 +9,30 @@ namespace GradeBook5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var book = new Book("Frank's Grade Book.");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
+            var stats = book.GetStatistics();
+           
+            Console.WriteLine($"The lowest grade recorded was {stats.Low}.");
+            Console.WriteLine($"The highest greade recorder was {stats.High}.");
+            Console.WriteLine($"The average grade is {stats.Average:N1}.");
+
+
+
+            /*if (args.Length > 0)
+            {
+               
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Just Hello");
+            }*/
+
+            Console.ReadLine();
         }
     }
 }
